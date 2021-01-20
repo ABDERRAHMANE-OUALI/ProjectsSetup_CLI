1b187104d405626368422d385e736eef29793503
ANDRUXUIS <abdealex05@gmail.com>
2021-01-20 15:21:41 +0100

    adding firebase to the cli interface options

--- script/push_to_github.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "adding firebase to the cli interface options"
-git push origin master -f
---
+++ scripts/push_to_github.sh
@@ -0,0 +1,3 @@
+git add .
+git commit -m "adding firebase to the cli interface options"
+git push origin master -f
