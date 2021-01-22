4086081a4598d064de2a0af6cc1d1a0dd14fffd1
ANDRUXUIS <abdealex05@gmail.com>
2021-01-22 19:00:29 +0100

    Publishing to npm registery

--- cmd/Nextjs.go
+++ cmd/Nextjs.go
@@ -3,4 +3,5 @@ package cmd
 // Nextjs function
 func Nextjs()  {
 	SystemCommand("yarn create next-app . && touch tsconfig.json && yarn add --dev typescript @types/react @types/node && yarn add sass")		
+	SystemCommand("cd pages/ && rename -v 's/ts/tsx/' * && rename -v 's/ts/tsx/' ./api/*")		
 }
